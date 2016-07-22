420439b9be581e48e096cbb56d0c4e80856b8da5
Arjun Singla <arjun0999@gmail.com>
2016-07-22 13:03:07 -0700

    committing current work, then going to revert the previous 6 commits

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716005608) do
+ActiveRecord::Schema.define(version: 20160722032955) do
 
   create_table "pieces", force: :cascade do |t|
     t.string   "title"
     t.string   "genre"
     t.string   "size"
-    t.decimal  "price"
+    t.decimal  "price",              precision: 8, scale: 2
     t.string   "description"
     t.integer  "status"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
