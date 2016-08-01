6c791041ac16b92ec1d9e5023ecd0a8142e20272
Arjun Singla <arjun0999@gmail.com>
2016-08-01 13:33:29 -0700

    Added customized mailers for buyers and sellers

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,9 +1,16 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	default from: "Metallic Palette <reciepts@metallicpalette.com>"
+	default from: "Metallic Palette <noreply@metallicpalette.com>"
 
 	def purchase_receipt purchase
 		@purchase = purchase
+		@piece = Piece.find(purchase.piece_id)
 		mail to: purchase.customer_email, subject: "Thanks for your purchase!"
 	 end
+
+	def purchase_notification purchase
+		@purchase = purchase
+		@piece = Piece.find(purchase.piece_id)
+		mail to: purchase.seller_email, subject: "You Just $old $omething!!"
+	end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,9 +1,13 @@
 class Purchase < ActiveRecord::Base
-	after_create :email_purchaser
+	after_create :email_purchaser, :email_seller
 
 	private
 
 	def email_purchaser
 		PurchaseMailer.purchase_receipt(self).deliver
+	end
+
+	def email_seller
+		PurchaseMailer.purchase_notification(self).deliver
 	end
 end
