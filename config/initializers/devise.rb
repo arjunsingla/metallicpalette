174eb017f122db19f241d7b1048b83f167f1ecbf
Arjun Singla <arjun0999@gmail.com>
2016-08-02 09:58:20 -0700

    Edited address of devise emails and put danger panel for deleting account

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@metallicpalette.com'
+  config.mailer_sender = '"Metallic Palette" <noreply@metallicpalette.com>'
+
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
