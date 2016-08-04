e5764c8e66eeba7587cad447a0e07a7c5c92ef59
Arjun Singla <arjun0999@gmail.com>
2016-08-04 11:49:44 -0700

    Customized ActiveAdmin Table and fixed border radius on alert

--- app/admin/purchase.rb
+++ app/admin/purchase.rb
@@ -13,5 +13,57 @@ ActiveAdmin.register Purchase do
 #   permitted
 # end
 
+	 filter :amount
+    filter :description
+    filter :piece_id
+    filter :customer_email
+    filter :seller_email
+    filter :customer_name
+    filter :customer_address_line_1
+    filter :customer_city
+    filter :customer_state
+    filter :customer_zip_code
+    filter :customer_country
+    filter :seller_name
+    filter :seller_address_line_1
+    filter :seller_address_line_2
+    filter :seller_city
+    filter :seller_state
+    filter :seller_zip_code 
+    filter :currency
+    filter :card
+    filter :created_at
+    filter :ship_by
+    filter :arrive_by
 
+
+	index do
+	    selectable_column
+	    id_column
+	    column :amount do |purchase|
+	    	"$" + number_with_precision((purchase.amount.to_f)/100, :precision => 2, :delimiter => ',')
+	     end
+	    column :description
+	    column :piece_id
+	    column :customer_email
+	    column :seller_email
+	    column :customer_name
+	    column :customer_address_line_1
+	    column :customer_city
+	    column :customer_state
+	    column :customer_zip_code
+	    column :customer_country
+	    column :seller_name
+	    column :seller_address_line_1
+	    column :seller_address_line_2
+	    column :seller_city
+	    column :seller_state
+	    column :seller_zip_code 
+	    column :currency
+	    column :card
+	    column :created_at
+	    column :ship_by
+	    column :arrive_by
+	    actions
+	  end
 end
