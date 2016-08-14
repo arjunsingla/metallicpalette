81a0ce4355f03f2642adcb5832fab608d56d614e
Arjun Singla <arjun0999@gmail.com>
2016-08-14 15:54:47 -0700

    Added artist_cut and total_price to model and validations, javascript for calculating, and updated show view

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -99,6 +99,6 @@ class PiecesController < ApplicationController
     end
 
     def piece_params
-      params.require(:piece).permit(:title, :image, :genre, :size, :price, :description, :status)
+      params.require(:piece).permit(:title, :image, :genre, :size, :artist_cut, :total_price, :description, :status)
     end
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -8,11 +8,21 @@ class Piece < ActiveRecord::Base
 	validates :image, presence: true
 	validates :genre, presence: true
 	validates :size, presence: true
-	validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0.5, less_than: 1000000 }
+	validates :artist_cut, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0.5, less_than: 443378.86 }
+	validates :total_price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: true
+	validate :total_price_validation
 	validates :description, presence: true
 
-	def price_in_cents
-		(self.price * 100).to_i
+
+	def total_price_in_cents
+		(self.total_price * 100).to_i
 	end
+
+
+	def total_price_validation
+		return if self.total_price == ((((self.artist_cut*2.19)+0.3)/(1-0.029))*100).ceil/100.0
+		errors.add(:total_price, "cannot be changed")
+	end
+
 
 end
